5aaba42d6441e17bb998ad19242016893ba18fec
PastLeo <chgu82837@gmail.com>
2014-11-19 16:48:59 +0800

    remove permission fix

--- fix_permission.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-chmod 775 -Rvf $PWD
-chmod 4750 -v "${BASH_SOURCE[0]}"
-chmod 4750 -v "$PWD/installUpdate.sh"
-
-# we need root permission to fix permission
-# So use root to give special permission to normal user
-# chmod 4750 fix_permission.sh 
