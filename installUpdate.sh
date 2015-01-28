a2ed6220b10b6233f3458569374c19d6e66e3b5c
PastLeo <chgu82837@gmail.com>
2015-01-28 17:12:23 +0800

    add clean up script

--- installUpdate.sh
+++ installUpdate.sh
@@ -11,9 +11,10 @@ if [ -z "$1" ]; then
 fi
 dspace_src="$( cd "$( dirname "$0" )" && pwd )"
 log="$(cd $(dirname $1); pwd)/$(basename $1)"
+mkdir -p $log
 
 echo "Src location: $dspace_src"
-echo "Log location: $log"
+echo "Build Record location: $log"
 echo "Is this OK? [Y/n]"
 read ans
 case "$ans" in
@@ -23,17 +24,21 @@ esac
 
 cd $dspace_src
 
-echo "=========== mvn package ===========" | tee $log
-mvn -U clean package | tee $log
+echo "=========== mvn package ===========" | tee -a $log/mvn.log
+mvn -U clean package | tee -a $log/mvn.log
 if [[ $? -ne 0 ]]; then
-	echo "mvn failed!" | tee $log
+	echo "mvn failed!" | tee -a $log/err.log
 fi;
 
 cd ${dspace_src}/dspace/target/dspace-*-build/
 if [[ $? -ne 0 ]]; then
-	echo "cd into ${dspace_src}/dspace/target/dspace-*-build/ failed!" | tee $log
+	echo "cd into ${dspace_src}/dspace/target/dspace-*-build/ failed!" | tee -a $log/err.log
 fi;
 
-echo "=========== ant update ===========" | tee $log
-ant update | tee $log
+echo "=========== ant update ===========" | tee -a $log/ant.log
+ant update | tee -a $log/ant.log
 
+echo "=========== cleaning up ===========" | tee -a $log/cleanup.log
+mkdir -p $log/bak $log/config-bak
+for k in $(locate -b .bak- | grep $dspace_src);do mv -v $k $log/bak/ | tee -a $log/cleanup.log; done;
+for k in $(locate -b .old | grep $dspace_src);do mv -v $k $log/config-bak/ | tee -a $log/cleanup.log; done;
